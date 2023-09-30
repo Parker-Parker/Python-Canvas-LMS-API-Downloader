38fc93fdda98f994b5a2a23d99d48827c1ed1a45
Parker-Parker <pagrant@wpi.edu>
2023-09-30 10:02:34 -0400

    threaded function for json window

--- Scripts/json_viewer.py
+++ Scripts/json_viewer.py
@@ -1,10 +1,6 @@
 import json
-import requests
-
-# Importing tkinter
+import threading
 from tkinter import * 
- 
-# Importing ttk from tkinter
 from tkinter import ttk 
  
 
@@ -13,111 +9,28 @@ from cached_requests import request_cached
 import tree_view
 
 
+def new_window(data):
+    x = threading.Thread(target=view_this, args=(data))
+    x.start()
+    return x
 
 
-# Creating app window
-app = Tk()
-app.rowconfigure(0, weight=1)
-app.columnconfigure(0, weight=1)
-app.geometry("1000x1000")
+def view_this(data):
+   # Creating app window
+    app = Tk()
+    app.rowconfigure(0, weight=1)
+    app.columnconfigure(0, weight=1)
+    app.geometry("1000x1000")
 
- 
-# Defining title of the app
-app.title("GUI JSON viewer ") 
+    
+    # Defining title of the app
+    app.title("GUI JSON viewer ") 
 
 
 
+    print(json.dumps(data))
 
-# # sample data
-
-# f = open('Downloads/cached_requests.json')
-# data = json.load(f)
-# f.close()
-
-data = request_cached("http://ip-api.com/json/23.43.252.19")
-
-# data = request_cached('https://api.github.com/events')
-
-# # end sample data
-
-print(json.dumps(data))
-
-tree_view.create_treeview(app,data)
- 
-# Calling main() 
-app.mainloop()
-
-#print(request_cached("http://ip-api.com/json/23.43.252.19"))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# # r = requests.get('https://api.github.com/events')
-# # print("\n===newline===\n")
-# # print(r.text)
-
-# # print("\n===newline===\n")
-# # print(r.json()[])
-
-# # print("\n===newline===\n")
-# # print(r.json())
-# # print("\n===newline===\n")
-# # print(json.dumps(r.json(),indent=2))
-# # print("\n===newline===\n")
-
-
-
-
-
-# r = requests.get('https://api.github.com/events')
-# rj = r.json()
-
-# print(r.headers)
-
-# print(len(rj))       # <-- List
-# print("\n\n\n") 
-
-# walmart_IP = "23.43.252.19"
-# geo_data = requests.get(f'http://ip-api.com/json/{walmart_IP}').json()
-# print(geo_data.keys())             # <-- object
-# print(geo_data["status"])
-
-
-
-
-# # http://ip-api.com/json/23.43.252.19
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    tree_view.create_treeview(app,data)
+     
+    app.mainloop()
 
